07cc1bf2e9b195c337304a193e2e6d634d22ff66
Nathan Patton <npatton@gmail.com>
2021-02-25 21:52:04 -0600

    configuration for packaging

---
+++ pythonbible_parser/__init__.py
@@ -0,0 +1,6 @@
+"""
+pythonbible-parser is a Python library for parsing Bible texts in various formats
+and convert them into a format for easy and efficient use in Python.
+"""
+
+__version__ = "0.0.1"
