437ce6230014fa693311457f8e1079ba42559081
Nathan Patton <npatton@gmail.com>
2022-07-09 18:01:23 -0500

    missed parentheses on lru_cache decorator

--- pythonbible_parser/osis/util.py
+++ pythonbible_parser/osis/util.py
@@ -7,27 +7,27 @@ from pythonbible import Book
 from pythonbible_parser.osis.constants import get_book_by_id
 
 
-@lru_cache
+@lru_cache()
 def get_namespace(tag: str) -> str:
     return tag[tag.index("{") + 1 : tag.index("}")]
 
 
-@lru_cache
+@lru_cache()
 def strip_namespace_from_tag(tag: str) -> str:
     return tag.replace(get_namespace(tag), "").replace("{", "").replace("}", "")
 
 
-@lru_cache
+@lru_cache()
 def get_element_text_and_tail(element: Element) -> str:
     return get_element_text(element) + get_element_tail(element)
 
 
-@lru_cache
+@lru_cache()
 def get_element_text(element: Element) -> str:
     return element.text.replace("\n", " ") if element.text else ""
 
 
-@lru_cache
+@lru_cache()
 def get_element_tail(element: Element) -> str:
     return element.tail.replace("\n", " ") if element.tail else ""
 
